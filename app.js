86ccd91b92796ac39758300a4006582e54f6a3ef
Stef <54073151+stefbartieri@users.noreply.github.com>
2025-03-17 13:59:19 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+let AmigosSecretros = [];
+
+
+function adicionarAmigo() {
+  let input = document.getElementById("amigo");
+  let nomes = input.value.trim("");
+
+  if (nomes === "") {
+    alert("preencha o campo com o nome do seus amigos");
+    return;
+  }
+  AmigosSecretros.push(nomes);
+  AtualizaListaAmigos();
+}
+
+function AtualizaListaAmigos() {
+  let list = document.getElementById("listaAmigos");
+  list.innerHTML = "";
+
+  for (let i = 0; i < AmigosSecretros.length; i++) {
+    let li = document.createElement("li");
+    li.textContent = AmigosSecretros[i];
+    list.appendChild(li);
+  }
+}
+
+function sortearAmigo() {
+    if(AmigosSecretros.length === 0){
+        alert("Sua lista de amigos esta vazia! Adicione algum amigo")
+        return
+    }
+    let sorteio= Math.floor(Math.random() * AmigosSecretros.length);
+    let amigoSorteado = AmigosSecretros[sorteio];
+
+    document.getElementById("resultado").innerHTML = `Seu amigo é <strong> ${amigoSorteado}<strong>`
+}
---
+++ readme.md
@@ -0,0 +1,17 @@
+# Sorteador de Amigo Secreto
+
+É um sorteador de amigo secreto para uso de qualquer pessoa.
+
+## Funcionalidades
+
+- **Adicionar Amigo:** Permite adicionar os nomes de vários amigos à lista de participantes do sorteio.
+- **Listar Amigos:** Exibe a lista de amigos adicionados.
+- **Sortear Amigo:** Realiza um sorteio aleatório de amigos secretos.
+- **Exibir Resultado:** É o resultado do sorteio.
+
+## Tecnologias utilizadas
+
+- HTML: Estrutura da página web.
+- CSS: Estilização da página web.
+- JavaScript: Funcionalidade do sorteio e manipulação da lista de amigos.
+
