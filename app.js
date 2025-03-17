cb2dd408fb1eb2abe5b67398e6a2b88f65aae92f
Stef <54073151+stefbartieri@users.noreply.github.com>
2025-03-17 14:01:19 -0300

    correção.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let AmigosSecretros = [];
+let AmigosSecretos = [];
 
 
 function adicionarAmigo() {
@@ -9,7 +9,7 @@ function adicionarAmigo() {
     alert("preencha o campo com o nome do seus amigos");
     return;
   }
-  AmigosSecretros.push(nomes);
+  AmigosSecretos.push(nomes);
   AtualizaListaAmigos();
 }
 
@@ -17,19 +17,19 @@ function AtualizaListaAmigos() {
   let list = document.getElementById("listaAmigos");
   list.innerHTML = "";
 
-  for (let i = 0; i < AmigosSecretros.length; i++) {
+  for (let i = 0; i < AmigosSecretos.length; i++) {
     let li = document.createElement("li");
-    li.textContent = AmigosSecretros[i];
+    li.textContent = AmigosSecretos[i];
     list.appendChild(li);
   }
 }
 
 function sortearAmigo() {
-    if(AmigosSecretros.length === 0){
+    if(AmigosSecretos.length === 0){
         alert("Sua lista de amigos esta vazia! Adicione algum amigo")
         return
     }
-    let sorteio= Math.floor(Math.random() * AmigosSecretros.length);
+    let sorteio= Math.floor(Math.random() * AmigosSecretos.length);
     let amigoSorteado = AmigosSecretros[sorteio];
 
     document.getElementById("resultado").innerHTML = `Seu amigo é <strong> ${amigoSorteado}<strong>`
